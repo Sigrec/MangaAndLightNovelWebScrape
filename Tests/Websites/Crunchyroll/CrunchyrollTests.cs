d4d5ba512e792c4a49bf5065bc574536979a32c3
Sigrec <seanisaacnjenga@gmail.com>
2023-12-24 16:56:47 -0800

    v1.1.4 Pre-Push
    Check changelog, still need to do test other websites and make small changes then will release v1.1.4

--- Tests/Websites/Crunchyroll/CrunchyrollTests.cs
+++ Tests/Websites/Crunchyroll/CrunchyrollTests.cs
@@ -15,102 +15,124 @@ namespace Tests.Websites
             WebsiteList = new HashSet<Website>() {Website.Crunchyroll};
         }
 
+        [Test, Description("Validates Series w/ Non Letter or Digit Char in Title")]
+        public async Task Crunchyroll_AkaneBanashi_Manga_Test()
+        {
+            await Scrape.InitializeScrapeAsync("Akane-Banashi", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
+            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollAkaneBanashiMangaData.txt")));
+        }
+
+        [Test, Description("Validates Series w/ Vol 0 that does not contain 'Vol' stirng & Novel entries")]
+        public async Task Crunchyroll_JujutsuKaisen_Manga_Test()
+        {
+            await Scrape.InitializeScrapeAsync("jujutsu kaisen", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
+            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollJujutsuKaisenMangaData.txt")));
+        }
+
         [Test, Description("Test Title that contains a keyword to skip and contains ':'")]
         public async Task Crunchyroll_AdventuresOfDai_Manga_Test()
         {
-            await Scrape.InitializeScrapeAsync("Dragon Quest: The Adventure of Dai", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
+            await Scrape.InitializeScrapeAsync("Dragon Quest: The Adventure of Dai", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
             Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollAdventuresOfDaiMangaData.txt")));
         }
 
         [Test, Description("Tests Manga book, Box Sets, Omnibus, & Manga w/ No Vol Number")]
         public async Task Crunchyroll_OnePiece_Test()
         {
-            await Scrape.InitializeScrapeAsync("one piece", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
+            await Scrape.InitializeScrapeAsync("One Piece", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
             Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollOnePieceMangaData.txt")));
         }
 
         [Test]
         public async Task Crunchyroll_Naruto_Manga_Test()
         {
-            await Scrape.InitializeScrapeAsync("Naruto", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
+            await Scrape.InitializeScrapeAsync("Naruto", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
             Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollNarutoMangaData.txt")));
         }
 
         [Test]
         public async Task Crunchyroll_Naruto_Novel_Test()
         {
-            await Scrape.InitializeScrapeAsync("Naruto", BookType.LightNovel, Array.Empty<StockStatus>(), WebsiteList);
+            await Scrape.InitializeScrapeAsync("Naruto", BookType.LightNovel, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
             Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollNarutoNovelData.txt")));
         }
 
         [Test]
         public async Task Crunchyroll_Bleach_Test()
         {
-            await Scrape.InitializeScrapeAsync("Bleach", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
+            await Scrape.InitializeScrapeAsync("Bleach", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
             Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollBleachMangaData.txt")));
         }
 
-        [Test, Description("Validates Novel w/ Manga Entries & Volume Numbers with Decimals")]
-        public async Task Crunchyroll_COTE_Novel_Test()
+        [Test, Description("Validates Series w/ dif Types of Omnibus & Box Set Entries")]
+        public async Task Crunchyroll_AttackOnTitan_Manga_Test()
         {
-            await Scrape.InitializeScrapeAsync("classroom of the elite", BookType.LightNovel, Array.Empty<StockStatus>(), WebsiteList);
-            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollCOTENovelData.txt")));
-        }
-
-        [Test, Description("Validates Manga w/ Novel Entries")]
-        public async Task Crunchyroll_COTE_Manga_Test()
-        {
-            await Scrape.InitializeScrapeAsync("classroom of the elite", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
-            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollCOTEMangaData.txt")));
-        }
-
-        [Test, Description("Validates Series w/ Number")]
-        public async Task Crunchyroll_DimensionalSeduction_Manga_Test()
-        {
-            await Scrape.InitializeScrapeAsync("2.5 Dimensional Seduction", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
-            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollDimensionalSeductionMangaData.txt")));
-        }
-
-        [Test, Description("Validates Series w/ Non Letter or Digit Char in Title")]
-        public async Task Crunchyroll_AkaneBanashi_Manga_Test()
-        {
-            await Scrape.InitializeScrapeAsync("Akane-Banashi", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
-            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollAkaneBanashiMangaData.txt")));
-        }
-
-        [Test, Description("Validates Series w/ Non Letter or Digit Char & Numbers in Title")]
-        public async Task Crunchyroll_07Ghost_Manga_Test()
-        {
-            await Scrape.InitializeScrapeAsync("07-ghost", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
-            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\Crunchyroll07GhostMangaData.txt")));
-        }
-
-        [Test, Description("Validates Manga Series w/ Deluxe Hardcover & Paperback Volumes")]
-        public async Task Crunchyroll_Berserk_Manga_Test()
-        {
-            await Scrape.InitializeScrapeAsync("Berserk", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
-            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollBerserkMangaData.txt")));
-        }
-
-        [Test, Description("Validates Manga Series w/ Hardcover & Paperback Volumes & Imperfect Volume")]
-        public async Task Crunchyroll_FMAB_Manga_Test()
-        {
-            await Scrape.InitializeScrapeAsync("fullmetal alchemist", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
-            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollFMABMangaData.txt")));
+            await Scrape.InitializeScrapeAsync("attack on titan", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
+            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollAttackOnTitanMangaData.txt")));
         }
 
         [Test, Description("Validates One Shot Manga Series")]
         public async Task Crunchyroll_GoodbyeEri_Manga_Test()
         {
-            await Scrape.InitializeScrapeAsync("Goodbye, Eri", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
+            await Scrape.InitializeScrapeAsync("Goodbye, Eri", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
             Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollGoodbyeEriMangaData.txt")));
         }
 
-         [Test, Description("Validates Series w/ dif Types of Omnibus & Box Set Entries")]
-        public async Task Crunchyroll_AttackOnTitan_Manga_Test()
+        [Test, Description("Validates Series w/ Number")]
+        public async Task Crunchyroll_DimensionalSeduction_Manga_Test()
         {
-            await Scrape.InitializeScrapeAsync("attack on titan", BookType.Manga, Array.Empty<StockStatus>(), WebsiteList);
-            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollAttackOnTitanMangaData.txt")));
+            await Scrape.InitializeScrapeAsync("2.5 Dimensional Seduction", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
+            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollDimensionalSeductionMangaData.txt")));
+        }
+
+        [Test, Description("Validates Novel w/ Novel after Volume and lowercase")]
+        public async Task Crunchyroll_Overlord_Novel_Test()
+        {
+            await Scrape.InitializeScrapeAsync("Overlord", BookType.LightNovel, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
+            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollOverlordNovelData.txt")));
+        }
+
+        // Currently there A La Carte are under Subject Comic and not Manga so theyh aren't grabbed
+        [Test, Description("Validates Manga w/ Novel Entries & Has Paperback & Hardcover Initially")]
+        public async Task Crunchyroll_Overlord_Manga_Test()
+        {
+            await Scrape.InitializeScrapeAsync("overlord", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
+            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollOverlordMangaData.txt")));
+        }
+
+        [Test, Description("Validates Series w/ Non Letter or Digit Char & Numbers in Title")]
+        public async Task Crunchyroll_07Ghost_Manga_Test()
+        {
+            await Scrape.InitializeScrapeAsync("07-ghost", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
+            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\Crunchyroll07GhostMangaData.txt")));
+        }
+
+        [Test, Description("Validates Manga Series w/ Hardcover & Paperback Volumes & Imperfect Volume")]
+        public async Task Crunchyroll_FMAB_Manga_Test()
+        {
+            await Scrape.InitializeScrapeAsync("fullmetal alchemist", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
+            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollFMABMangaData.txt")));
+        }
+
+        [Test, Description("Validates Manga Series w/ Deluxe Hardcover & Paperback Volumes")]
+        public async Task Crunchyroll_Berserk_Manga_Test()
+        {
+            await Scrape.InitializeScrapeAsync("Berserk", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
+            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollBerserkMangaData.txt")));
+        }
+
+        [Test, Description("Validates Manga Series w/ Box Set with Specific name")]
+        public async Task Crunchyroll_ToiletBound_Manga_Test()
+        {
+            await Scrape.InitializeScrapeAsync("Toilet-bound Hanako-kun", BookType.Manga, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
+            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollToiletMangaData.txt")));
+        }
+
+        [Test, Description("Validates Novel w/ Manga Entries & Volume Numbers with Decimals")]
+        public async Task Crunchyroll_COTE_Novel_Test()
+        {
+            await Scrape.InitializeScrapeAsync("classroom of the elite", BookType.LightNovel, MasterScrape.EXCLUDE_NONE_FILTER, WebsiteList);
+            Assert.That(Scrape.GetResults(), Is.EqualTo(ImportDataToList(@"C:\MangaAndLightNovelWebScrape\Tests\Websites\Crunchyroll\CrunchyrollCOTENovelData.txt")));
         }
     }
 }
